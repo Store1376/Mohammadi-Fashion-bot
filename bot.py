2056f0ed7fba7e0dc5e0fbdcb3833d241acc104d
Store1376 <roh.moh1375@gmail.com>
2026-09-19 21:50:06 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,10 +6,8 @@ from telegram.ext import Application, CommandHandler, CallbackQueryHandler, Cont
 TOKEN = "8850373531:AAHe0CfB5f2-C382MtqG3wt0RxBLvjXFO8Q"
 
 # بعداً شناسه عددی خودت را اینجا قرار می‌دهیم
-ADMIN_ID = ⚙️ مدیریت فروشگاه
 
-🆔 شناسه عددی شما:
-8276323231
+ADMIN_ID = 8276323231
 
 این عدد را برای خودت نگه دار.
 
